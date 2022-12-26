d9bd90d8b2676a7b03d7187562829e72a2fdb245
Moises Castillo <moisescastillo20@gmail.com>
2022-12-26 10:25:14 +0100

    feat: adding new packages and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { Route, Routes } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+import { ReactQueryDevtools } from 'react-query/devtools';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query';
+
 function App() {
+
+  const queryClient = new QueryClient();
+
   return (
-    <div>
-      App
-    </div>
+    <QueryClientProvider client={queryClient}>
+    <Routes>
+      <Route path="/" element={<MainPage />} />
+      <Route path="/podcast/:podcastId" element={<h1>Detalle de un podcast</h1>} />
+      <Route path="/podcast/:podcastId/episode/:episodeId" element={<h1>Detalle de un episodio</h1>} />
+    </Routes>
+    <ReactQueryDevtools initialIsOpen={false} />
+     </QueryClientProvider>
   );
 }
 
