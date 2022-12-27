3c6261da9301c175e999e102baaf418a127c37e2
Moises Castillo <moisescastillo20@gmail.com>
2022-12-27 10:13:32 +0100

    refactor: refactoring for context

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import { ReactQueryDevtools } from 'react-query/devtools';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from 'react-query';
+import DetailPodcast from "./pages/DetailPodcast";
 
 function App() {
 
-  const queryClient = new QueryClient();
-
   return (
-    <QueryClientProvider client={queryClient}>
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route path="/podcast/:podcastId" element={<h1>Detalle de un podcast</h1>} />
-      <Route path="/podcast/:podcastId/episode/:episodeId" element={<h1>Detalle de un episodio</h1>} />
-    </Routes>
-    <ReactQueryDevtools initialIsOpen={false} />
-     </QueryClientProvider>
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+    <Route path="/podcast/:podcastId" element={<DetailPodcast/>} />
+    <Route path="/podcast/:podcastId/episode/:episodeId" element={<h1>Detalle de un episodio</h1>} />
+  </Routes>
   );
 }
 
